7dd80c320c6bf82c527bcc9578dd865bf3bd7b9d
xuemeng <xuemeng2@ualberta.ca>
2023-11-14 03:26:27 -0700

    add lab

--- authlab/settings.py
+++ authlab/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'client.apps.ClientConfig',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -128,5 +129,12 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+
     )
 }
+
+# settings.py
+
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
